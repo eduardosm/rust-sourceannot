22c1d9b60d4edd4f1ec7d2fdc75b3c90b244b767
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-03-26 20:54:48 +0100

    chore: add badges to README

--- ci/check-msrv.sh
+++ ci/check-msrv.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 msrv="$(cat ci/rust-versions/msrv.txt)"
 msrv="${msrv%.*}"
 
+if [[ "$(grep img.shields.io/badge/rustc README.md)" != *"rustc-$msrv+-lightgray.svg"* ]]; then
+  echo "Incorrect MSRV in README.md"
+  exit 1
+fi
+
 if [ "$(grep rust-version Cargo.toml)" != "rust-version = \"$msrv\"" ]; then
   echo "Incorrect rust-version in Cargo.toml"
   exit 1
