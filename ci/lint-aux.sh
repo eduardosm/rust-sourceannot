df99a2523731fee1d86b944a440ce5afe8c83490
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-03-27 17:29:50 +0100

    ci: add job to run auxiliary lints
    Uses markdownlint and shellcheck. MSRV consistency checks are now part of this job.
    
    This commits also includes fixes for warnings in shell scripts and markdown documents detected by the linters.

--- ci/lint-aux.sh
+++ ci/lint-aux.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Checking MSRV consistency"
+
 msrv="$(cat ci/rust-versions/msrv.txt)"
 msrv="${msrv%.*}"
 
@@ -13,3 +15,9 @@ if [ "$(grep rust-version Cargo.toml)" != "rust-version = \"$msrv\"" ]; then
   echo "Incorrect rust-version in Cargo.toml"
   exit 1
 fi
+
+echo "Checking shell scripts with shellcheck"
+find . -type f -name "*.sh" -not -path "./.git/*" -print0 | xargs -0 shellcheck
+
+echo "Checking markdown documents with markdownlint"
+find . -type f -name "*.md" -not -path "./.git/*" -print0 | xargs -0 mdl
