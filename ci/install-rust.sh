36d589cc0bcb8c5991051ad4a2258436ff2b66a8
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-03-27 17:29:50 +0100

    ci: add job to run auxiliary lints
    Uses markdownlint and shellcheck. MSRV consistency checks are now part of this job.
    
    This commits also includes fixes for warnings in shell scripts and markdown documents detected by the linters.

--- ci/install-rust.sh
+++ ci/install-rust.sh
@@ -5,9 +5,9 @@ rust_version="$1"
 shift
 
 if [[ "$rust_version" = *.txt ]]; then
-  rust_version="$(cat ci/rust-versions/$rust_version)"
+  rust_version="$(cat "ci/rust-versions/$rust_version")"
 fi
 
 echo "Installing Rust $rust_version"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain "$rust_version" "$@"
-echo "$HOME/.cargo/bin" >> $GITHUB_PATH
+echo "$HOME/.cargo/bin" >> "$GITHUB_PATH"
