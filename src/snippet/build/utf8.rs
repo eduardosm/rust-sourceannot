4f0945695225f0e942aa38a0b6a024b2f6cf66af
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-04-20 17:45:44 +0200

    refactor: move UTF-8 snippet building to its own submodule

--- src/snippet/build/utf8.rs
+++ src/snippet/build/utf8.rs
@@ -1,95 +1,5 @@
-use super::{SourceLine, SourceSnippet, SourceUnitMeta};
-use crate::range_set::RangeSet;
-
-struct SourceSnippetBuilder {
-    start_line: usize,
-    lines: Vec<SourceLine>,
-    line_map: Vec<usize>,
-    metas: Vec<SourceUnitMeta>,
-    current_line_text: String,
-    current_line_alts: RangeSet<usize>,
-    current_line_width: usize,
-}
-
-impl SourceSnippetBuilder {
-    fn new(start_line: usize) -> Self {
-        Self {
-            start_line,
-            lines: Vec::new(),
-            line_map: Vec::new(),
-            metas: Vec::new(),
-            current_line_text: String::new(),
-            current_line_alts: RangeSet::new(),
-            current_line_width: 0,
-        }
-    }
-
-    fn finish(mut self) -> SourceSnippet {
-        self.lines.push(SourceLine {
-            text: self.current_line_text.into_boxed_str(),
-            alts: self.current_line_alts,
-            width: self.current_line_width,
-        });
-
-        SourceSnippet {
-            start_line: self.start_line,
-            lines: self.lines,
-            line_map: self.line_map,
-            metas: self.metas,
-        }
-    }
-
-    fn next_line(&mut self, extra_widths: &[usize]) {
-        self.lines.push(SourceLine {
-            text: std::mem::take(&mut self.current_line_text).into_boxed_str(),
-            alts: std::mem::take(&mut self.current_line_alts),
-            width: std::mem::replace(&mut self.current_line_width, 0),
-        });
-        self.metas
-            .extend(extra_widths.iter().map(|&w| SourceUnitMeta::new(w, 0)));
-        self.line_map.push(self.metas.len());
-    }
-
-    fn push_text(&mut self, text: &str, orig_len: usize, alt: bool) {
-        let old_line_len = self.current_line_text.len();
-        self.current_line_text.push_str(text);
-        let new_line_len = self.current_line_text.len();
-
-        if alt && !text.is_empty() {
-            self.current_line_alts
-                .insert(old_line_len..=(new_line_len - 1));
-        }
-
-        let width = unicode_width::UnicodeWidthStr::width(text);
-        self.current_line_width += width;
-
-        self.metas.push(SourceUnitMeta::new(width, text.len()));
-        for _ in 1..orig_len {
-            // Each element of `snippet.widths` corresponds to a byte in `source`,
-            // so fill with -1 for multi-unit chunks.
-            self.metas.push(SourceUnitMeta::extra());
-        }
-    }
-
-    fn push_char(&mut self, chr: char, width: usize, orig_len: usize, alt: bool) {
-        let old_line_len = self.current_line_text.len();
-        self.current_line_text.push(chr);
-        let new_line_len = self.current_line_text.len();
-        self.current_line_width += width;
-
-        if alt {
-            self.current_line_alts
-                .insert(old_line_len..=(new_line_len - 1));
-        }
-
-        self.metas.push(SourceUnitMeta::new(width, chr.len_utf8()));
-        for _ in 1..orig_len {
-            // Each element of `snippet.widths` corresponds to a byte in `source`,
-            // so fill with -1 for multi-unit characters.
-            self.metas.push(SourceUnitMeta::extra());
-        }
-    }
-}
+use super::SourceSnippetBuilder;
+use crate::SourceSnippet;
 
 impl SourceSnippet {
     /// Creates a snippet from a UTF-8 (possibly broken) source.
@@ -203,7 +113,8 @@ impl SourceSnippet {
 
 #[cfg(test)]
 mod tests {
-    use super::{RangeSet, SourceLine, SourceSnippet, SourceUnitMeta};
+    use crate::range_set::RangeSet;
+    use crate::snippet::{SourceLine, SourceSnippet, SourceUnitMeta};
 
     fn meta(width: usize, len: usize) -> SourceUnitMeta {
         SourceUnitMeta::new(width, len)
@@ -214,7 +125,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_simple_1() {
+    fn test_simple_1() {
         let source = b"123\n456";
         let snippet = SourceSnippet::build_from_utf8_ex(
             0,
@@ -257,7 +168,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_simple_2() {
+    fn test_simple_2() {
         let source = b"123\n456\n";
         let snippet = SourceSnippet::build_from_utf8_ex(
             0,
@@ -306,7 +217,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_crlf() {
+    fn test_crlf() {
         let source = b"123\r\n4\r6\r\n";
         let snippet = SourceSnippet::build_from_utf8_ex(
             0,
@@ -357,7 +268,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_fullwidth() {
+    fn test_fullwidth() {
         let source = b"1\xEF\xBC\x923\n456";
         let snippet = SourceSnippet::build_from_utf8_ex(
             0,
@@ -401,7 +312,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_tabs() {
+    fn test_tabs() {
         let source = b"123\n\t456";
         let snippet = SourceSnippet::build_from_utf8(0, source, 4);
 
@@ -439,7 +350,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_invalid_single() {
+    fn test_invalid_single() {
         let source = b"1\xF1\x803\n456";
         let snippet = SourceSnippet::build_from_utf8_ex(
             0,
@@ -490,7 +401,7 @@ mod tests {
     }
 
     #[test]
-    fn test_utf8_invalid_multi() {
+    fn test_invalid_multi() {
         let source = b"1\xF1\x803\n456";
         let snippet = SourceSnippet::build_from_utf8_ex(
             0,
