d981e7be0ea5bbe1219b874cfc776ba3bc7a705f
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-04-23 00:01:17 +0200

    ci: install things in a different step

--- ci/clippy.sh
+++ ci/clippy.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 . ci/utils.sh
 
-begin_group "Install Rust"
-./ci/install-rust.sh stable.txt --profile minimal -c clippy
-# shellcheck disable=SC1091
-. "$HOME/.cargo/env"
-end_group
-
 begin_group "Fetch dependencies"
 cargo fetch --locked
 end_group
